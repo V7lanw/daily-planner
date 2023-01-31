113a8b952948ee62715df0b1092e38fa45ca416e
rbmilkveWin <v7lanw@hotmail.com>
2023-01-31 20:39:06 +0800

    fixed formatted date info display bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,13 +17,18 @@ $(document).ready(function () {
     function hourUpdate () {
         let currentHour = moment().hours();
         console.log(currentHour);
+        // One way of traverse all time blocks
         // $(".time-block").each(function () {
         //     let blockHour = parseInt($(this).attr("id").split("-")[1]);
         //     console.log(blockHour);
         // });
+        // Another way of traverse all time blocks
         for (let i = 0; i < $(".time-block").length; i++) {
             let blockHour = parseInt($(".time-block")[i].getAttribute("id").split("-")[1]);
             console.log(blockHour);
+            // Here, if you would like to test all the time blocks and current 
+            // time, you could change your system clock. That works for localhost, 
+            // or live server version, I don't know whether it works for GitHub pages.
             if (blockHour < currentHour) {
                 $(".time-block")[i].classList.add("past");
                 $(".time-block")[i].classList.remove("present");
@@ -53,5 +58,8 @@ $(document).ready(function () {
     $("#hour-16 .description").val(localStorage.getItem("hour-16"));
     $("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
-    $("#currentDay").text(moment().format("dddd, MMMM, Do"));
+    // I don't know why the text content is blank if I just set text using jQuery,
+    // but now the text content is the formatted date info.
+    let today = $("#currentDay").text(moment().format("dddd, MMMM, Do"));
+    console.log(today);
 });
